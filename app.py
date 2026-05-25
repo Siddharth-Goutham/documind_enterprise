97ba042f3a0793a6138591f6db4300b7ebf85ec4
Siddharth Goutham <siddhartha.nalluraya@gmail.com>
2026-05-25 19:28:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ load_dotenv()
 os.environ["HF_TOKEN"] = os.getenv("HF_TOKEN")
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = "SUPERSECRET"
+app.secret_key = os.getenv("SECRET_KEY", "dev_fallback_string_key_12345")
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 UPLOAD_FOLDER = os.path.join(BASE_DIR, "static", "files")
